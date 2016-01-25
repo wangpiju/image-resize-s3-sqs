61867b78390f47d417aa996bd08ef86f2492ed95
Jason Wang <wangpiju0420@gmail.com>
2016-01-25 21:23:28 +0800

    test

--- web/config.php
+++ web/config.php
@@ -1,7 +1,6 @@
 <?php
-putenv("AWS_ACCESS_KEY_ID=AKIAIBWJGGPCGPJLVJVA");
-putenv("AWS_SECRET_ACCESS_KEY=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");
-putenv("S3_BUCKET=imguploadwang2");
+
+
 $key = getenv('AWS_ACCESS_KEY_ID')?: die('No "AWS_ACCESS_KEY_ID" config var in found in env!');
 $secret = getenv('AWS_SECRET_ACCESS_KEY')?: die('No "AWS_SECRET_ACCESS_KEY" config var in found in env!');
 $default_bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
